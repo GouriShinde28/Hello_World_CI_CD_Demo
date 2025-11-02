df19feb32a14edb69b9f65668f633bc8997a1538
Gouri <gourishinde15@gmail.com>
2025-11-02 15:00:31 +0530

    Add tests for hello world

---
+++ HelloWorldTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testPrintHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHello();
+        String expectedOutput = "Hello, World!" + System.lineSeparator();
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
